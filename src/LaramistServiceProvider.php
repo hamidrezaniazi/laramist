b9627df5b2dd36c980edfea5809b9b14504f570d
Hamidreza Niazi <hamidrezanaizi@yahoo.com>
2019-10-15 01:45:04 +0330

    Remove service provider boilerplate code

--- src/LaramistServiceProvider.php
+++ src/LaramistServiceProvider.php
@@ -11,36 +11,7 @@ class LaramistServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laramist');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laramist');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
-        if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('laramist.php'),
-            ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/laramist'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/laramist'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/laramist'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
         }
     }
 
@@ -49,12 +20,6 @@ class LaramistServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laramist');
-
-        // Register the main class to use with the facade
-        $this->app->singleton('laramist', function () {
-            return new Laramist;
-        });
+        //
     }
 }
