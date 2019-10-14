52c1d9eda7273d9ac8aaab44407bb1231ee81c90
Hamidreza Niazi <hamidrezanaizi@yahoo.com>
2019-10-15 01:46:22 +0330

    Add model history

--- src/LaramistServiceProvider.php
+++ src/LaramistServiceProvider.php
@@ -11,7 +11,11 @@ class LaramistServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (! class_exists('CreateModelHistoriesTable')) {
             $this->publishes([
+                __DIR__ . '../database/migrations/create_model_histories_table.php.stub' =>
+                    database_path('migrations' . date('Y_m_d_His', time()) . 'create_model_histories_table.php')
+            ], 'migrations');
         }
     }
 
---
+++ tests/Migrations/CreateLaramistUsersTable.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Hamidrezaniazi\Laramist\Tests\Migrations;
+
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLaramistUsersTable
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('laramist_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('laramist_users');
+    }
+}
